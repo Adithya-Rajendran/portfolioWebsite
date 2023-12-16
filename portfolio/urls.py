38ac8a3dd1cebc71b298fc2598c7714c527477d0
Adithya Rajendran <adithyaraj@gmail.com>
2023-12-16 00:27:24 -0800

    Added the LinkedIn and GitHub redirects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('resume/', views.resume, name='resume'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
-    path('linkedin/', views.linkedin_redirect, name='linkedin_redirect'),
-    path('github/', views.github_redirect, name='github_redirect'),
+    path('linkedin/', views.linkedin_redirect, name='linkedin'),
+    path('github/', views.github_redirect, name='github'),
     path("", views.home, name="home"),
 ]
