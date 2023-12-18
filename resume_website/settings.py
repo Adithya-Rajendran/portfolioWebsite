04066a4079958e271cfa80db0e820aa3d1d69da2
Adithya Rajendran <adithyaraj@gmail.com>
2023-12-17 20:35:36 -0800

    Changed the static url

--- resume_website/settings.py
+++ resume_website/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/home/cagmas/PycharmProjects/portfolio_website/portfolio/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
