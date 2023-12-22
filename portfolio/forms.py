451e9bdb1f845712d34d341fcb82a2a77ac1b0ef
Adithya Rajendran <adithyaraj@gmail.com>
2023-12-21 17:35:34 -0800

    updated readability and added placeholders

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,9 +1,37 @@
 from django import forms
+from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 from phonenumber_field.formfields import PhoneNumberField
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    phone = PhoneNumberField(region="US", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'style': 'height: 10rem;'}))
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your Name'
+        })
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'email@domain.com'
+        })
+    )
+
+    phone = PhoneNumberField(
+        region="US",
+        widget=PhoneNumberInternationalFallbackWidget(attrs={
+            'class': 'form-control',
+            'placeholder': '+1 (555) 555-5555'}
+        ),
+        required=False
+    )
+
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'style': 'height: 10rem;',
+            'placeholder': 'Your Message'
+        })
+    )
