e4be9cc1b330d9d1c53c1506a17bda886be6637f
Adithya Rajendran <adithyaraj@gmail.com>
2024-01-11 12:55:31 -0800

    updated paths for new blog app

--- resume_website/urls.py
+++ resume_website/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("blogs/", include("blogs.urls")),
     path("", include("portfolio.urls")),
 ]
